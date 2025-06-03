db18bcf347dabdd8ef7becf9c8c9bc57b5b41021
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-06-03 14:29:18 +0530

    Initial backend setup

---
+++ backend/app/main.py
@@ -0,0 +1,18 @@
+# backend/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # allow frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Pharmacy API"}
