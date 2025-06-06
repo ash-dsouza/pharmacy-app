9ef52bbe93373a192b4f7d29040557f1b3571283
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-06-06 19:56:31 +0530

    Adding route to Contact Page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home'; // Make sure the import points to 'home.component.ts'
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
 // Export routes array only
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact }
 ];
