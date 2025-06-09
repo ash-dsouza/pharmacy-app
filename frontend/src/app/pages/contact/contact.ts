8a6c546edd2105b4bbe06c0b5d53b2d94c2af708
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-06-09 20:34:14 +0530

    Updating contact page

--- frontend/src/app/pages/contact/contact.ts
+++ frontend/src/app/pages/contact/contact.ts
@@ -1,23 +1,51 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../../services/contact.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule,FormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrls: ['./contact.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
 export class Contact {
-   contactForm = {
+  contactForm = {
     name: '',
     email: '',
     message: ''
   };
 
+  successMessage = '';
+  errorMessage = '';
+
+  constructor(private contactService: ContactService) {}
+
   submitForm() {
-    console.log('Form Submitted:', this.contactForm);
-    alert('Thanks for contacting us!');
-  }
+  this.contactService.sendMessage(this.contactForm).subscribe({
+    next: (res) => {
+      this.successMessage = 'Message sent successfully!';
+      this.errorMessage = '';
+      this.contactForm = { name: '', email: '', message: '' };
+
+      // Hide the success message after 3 seconds
+      setTimeout(() => {
+        this.successMessage = '';
+      }, 3000);
+    },
+    error: () => {
+      this.errorMessage = 'Something went wrong. Please try again.';
+      this.successMessage = '';
+
+      // Hide the error message after 3 seconds
+      setTimeout(() => {
+        this.errorMessage = '';
+      }, 3000);
+    }
+  });
+}
+
 
 }
